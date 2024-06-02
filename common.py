7ce9521ae81ff8db73319db6e3f140e4d34f6c98
zkhan <apps4av@gmail.com>
2024-06-02 11:51:27 -0400

    added sectionals

--- common.py
+++ common.py
@@ -87,12 +87,16 @@ def zip_files(list_of_all_tiles, chart):
     # US geo regions
 
     try:
+        os.remove("AK_" + chart + ".zip")
+        os.remove("HI_" + chart + ".zip")
         os.remove("NW_" + chart + ".zip")
         os.remove("SW_" + chart + ".zip")
         os.remove("NC_" + chart + ".zip")
         os.remove("SC_" + chart + ".zip")
         os.remove("NE_" + chart + ".zip")
         os.remove("SE_" + chart + ".zip")
+        os.remove("AK_" + chart)
+        os.remove("HI_" + chart)
         os.remove("NW_" + chart)
         os.remove("SW_" + chart)
         os.remove("NC_" + chart)
@@ -102,12 +106,16 @@ def zip_files(list_of_all_tiles, chart):
     except FileNotFoundError as e:
         pass
 
+    ak_file = zipfile.ZipFile("AK_" + chart + ".zip", "w")
+    hi_file = zipfile.ZipFile("HI_" + chart + ".zip", "w")
     nw_file = zipfile.ZipFile("NW_" + chart + ".zip", "w")
     sw_file = zipfile.ZipFile("SW_" + chart + ".zip", "w")
     nc_file = zipfile.ZipFile("NC_" + chart + ".zip", "w")
     sc_file = zipfile.ZipFile("SC_" + chart + ".zip", "w")
     ne_file = zipfile.ZipFile("NE_" + chart + ".zip", "w")
     se_file = zipfile.ZipFile("SE_" + chart + ".zip", "w")
+    ak_file_manifest = open("AK_" + chart, "w+")
+    hi_file_manifest = open("HI_" + chart, "w+")
     nw_file_manifest = open("NW_" + chart, "w+")
     sw_file_manifest = open("SW_" + chart, "w+")
     nc_file_manifest = open("NC_" + chart, "w+")
@@ -115,6 +123,8 @@ def zip_files(list_of_all_tiles, chart):
     ne_file_manifest = open("NE_" + chart, "w+")
     se_file_manifest = open("SE_" + chart, "w+")
 
+    ak_file_manifest.write(cycle.get_cycle() + "\n")
+    hi_file_manifest.write(cycle.get_cycle() + "\n")
     nw_file_manifest.write(cycle.get_cycle() + "\n")
     sw_file_manifest.write(cycle.get_cycle() + "\n")
     nc_file_manifest.write(cycle.get_cycle() + "\n")
@@ -122,12 +132,14 @@ def zip_files(list_of_all_tiles, chart):
     ne_file_manifest.write(cycle.get_cycle() + "\n")
     se_file_manifest.write(cycle.get_cycle() + "\n")
 
+    ak = (-180, 71, -126, 51)
+    hi = (-162, 24, -152, 18)
     nw = (-125, 50, -105, 40)
-    sw = (-125, 40, -105, 20)
+    sw = (-125, 40, -105, 15)
     nc = (-105, 50, -80,  40)
-    sc = (-105, 40, -80,  20)
+    sc = (-105, 40, -80,  15)
     ne = (-80,  50, -60,  40)
-    se = (-80,  40, -60,  20)
+    se = (-80,  40, -60,  15)
 
     for tile in tqdm(list_of_all_tiles, desc="Zipping up tiles in Areas"):
         tokens = tile.split("/")
@@ -136,6 +148,12 @@ def zip_files(list_of_all_tiles, chart):
         z_tile = int(tokens[len(tokens) - 3])
         lon_tile, lat_tile, lon1_tile, lat1_tile = projection.findBounds(x_tile, y_tile, z_tile)
         # include 7 and below in every chart
+        if is_in(ak, lon_tile, lat_tile) or z_tile <= 7:
+            ak_file.write(tile)
+            ak_file_manifest.write(tile + "\n")
+        if is_in(hi, lon_tile, lat_tile) or z_tile <= 7:
+            hi_file.write(tile)
+            hi_file_manifest.write(tile + "\n")
         if is_in(nw, lon_tile, lat_tile) or z_tile <= 7:
             nw_file.write(tile)
             nw_file_manifest.write(tile + "\n")
@@ -155,6 +173,8 @@ def zip_files(list_of_all_tiles, chart):
             se_file.write(tile)
             se_file_manifest.write(tile + "\n")
 
+    ak_file_manifest.close()
+    hi_file_manifest.close()
     nw_file_manifest.close()
     sw_file_manifest.close()
     nc_file_manifest.close()
@@ -163,6 +183,8 @@ def zip_files(list_of_all_tiles, chart):
     se_file_manifest.close()
 
     # write manifest
+    ak_file.write("AK_" + chart)
+    hi_file.write("HI_" + chart)
     nw_file.write("NW_" + chart)
     sw_file.write("SW_" + chart)
     nc_file.write("NC_" + chart)
@@ -170,6 +192,8 @@ def zip_files(list_of_all_tiles, chart):
     ne_file.write("NE_" + chart)
     se_file.write("SE_" + chart)
 
+    ak_file.close()
+    hi_file.close()
     nw_file.close()
     sw_file.close()
     nc_file.close()
---
+++ sec.py
@@ -0,0 +1,30 @@
+import common
+import glob
+
+import cycle
+
+
+# Chart specific code
+
+# download add second entry for caribbean charts
+start_date = cycle.get_version_start(cycle.get_cycle_download())  # to download which cycle
+all_charts = common.list_crawl("https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/vfr/", "^http.*" + start_date + "/sectional-files/.*.zip$")
+all_charts_2 = common.list_crawl("https://www.faa.gov/air_traffic/flight_info/aeronav/digital_products/vfr/", "^http.*" + start_date + "/Caribbean/.*.zip$")
+for nn in all_charts_2:
+    all_charts.append(nn)
+common.download_list(all_charts)
+all_files = common.get_files("*SEC.tif")
+all_files_2 = common.get_files("*VFR Chart.tif")
+for nn in all_files_2:
+    all_files.append(nn)
+# remove unused stuff
+all_files.remove("Honolulu Inset SEC.tif")
+
+# make tiles
+vrts = common.make_vrt_list(all_files, "SEC")
+common.make_main_vrt(vrts, "SEC")
+common.make_tiles("0", "10", "SEC")
+
+# zip
+all_tiles = glob.glob("tiles/0/**/*.webp", recursive=True)
+common.zip_files(all_tiles, "SEC")
